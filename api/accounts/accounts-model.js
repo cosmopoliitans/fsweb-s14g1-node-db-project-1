03bef4a4b69c8bf937ef82365d50b992d0c8b7b6
cosmopoliitans <elfkyn52@gmail.com>
2023-05-22 22:24:02 +0300

    görevler tamamlandı

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,35 @@
+const db = require("../../data/db-config");
+
 const getAll = () => {
   // KODLAR BURAYA
-}
+  return db("accounts");
+};
 
-const getById = id => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  return db("accounts").where("id", id).first();
+};
 
-const create = account => {
-  // KODLAR BURAYA
-}
+const getByName = (name) => {
+  return db("accounts").where("name", name).first();
+};
 
-const updateById = (id, account) => {
+const create = async (account) => {
   // KODLAR BURAYA
-}
+  const inserted = await db("accounts").insert(account);
+  return getById(inserted[0]);
+};
 
-const deleteById = id => {
+const updateById = async (id, account) => {
   // KODLAR BURAYA
-}
+  await db("accounts").where("id", id).update(account);
+  return getById(id);
+};
+
+const deleteById = (id) => {
+  // KODLAR BURAYA
+  return db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +37,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+  getByName,
+};
